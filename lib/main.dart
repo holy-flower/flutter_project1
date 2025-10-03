d82f5c0dca26e39d186c61e032f87bf5d2df37c9
holy-flower <prodolnova04@mail.ru>
2025-10-03 12:46:52 +0300

    Создание bottomNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CosmetologyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CosmetologyApp extends StatelessWidget {
+  const CosmetologyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: MySimpleWidget(),
-        ),
+      title: 'Салон Красоты "BeauiClinic"',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        fontFamily: 'Roboto',
       ),
+      home: const MainNavigationScreen(),
     );
   }
 }
 
-class MySimpleWidget extends StatefulWidget {
-  const MySimpleWidget({super.key});
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
 
   @override
-  State<MySimpleWidget> createState() => _MySimpleWidgetState();
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
 }
 
-class _MySimpleWidgetState extends State<MySimpleWidget> {
-  String _text = 'Нажми меня';
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _currentScreenIndex = 0;
 
-  void _changeText() {
+  final List<String> _screenTitles = [
+    'Уход за лицом',
+    'Уход за телом',
+    'Депиляция',
+    'Массаж',
+    'SPA-программы',
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      _text = 'Молодец';
+      _currentScreenIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: _changeText,
-      child: Text(_text),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_screenTitles[_currentScreenIndex]),
+        backgroundColor: Colors.pink[100],
+        elevation: 2,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentScreenIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.pink,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.face),
+            label: 'Лицо',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.self_improvement),
+            label: 'Тело',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.content_cut),
+            label: 'Депиляция',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.spa),
+            label: 'Массаж',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pool),
+            label: 'SPA',
+          ),
+        ],
+      ),
     );
   }
 }
