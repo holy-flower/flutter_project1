54220b560c436d7101747f0788292a2985ea031d
holy-flower <prodolnova04@mail.ru>
2025-10-24 19:28:10 +0300

    добавление сетевого изображения на экран массажа

--- lib/features/massage/screens/massage_screen.dart
+++ lib/features/massage/screens/massage_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/massage_service.dart';
 import '../widgets/massage_list.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class MassageScreen extends StatelessWidget {
   final List<MassageService> massages;
@@ -32,6 +33,20 @@ class MassageScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            CachedNetworkImage(
+              imageUrl: 'https://avatars.mds.yandex.net/get-altay/367512/2a0000015b861eefde2b7a30525f34bd1031/orig',
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              progressIndicatorBuilder: (context, url, progress) =>
+              const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Center(
+                child: Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+              ),
+            ),
             const SizedBox(height: 20),
             Expanded(
               child: MassageList(
