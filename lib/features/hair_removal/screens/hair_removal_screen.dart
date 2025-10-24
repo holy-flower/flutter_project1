82c0401e091ffb46d5af84fb3aa6adebc1e51ae5
holy-flower <prodolnova04@mail.ru>
2025-10-24 19:24:37 +0300

    добавление сетевого изображения на экран депиляции

--- lib/features/hair_removal/screens/hair_removal_screen.dart
+++ lib/features/hair_removal/screens/hair_removal_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/hair_removal_service.dart';
 import '../widgets/service_selector.dart';
 import '../widgets/service_details.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class HairRemovalScreen extends StatelessWidget {
   final List<HairRemovalService> services;
@@ -33,6 +34,20 @@ class HairRemovalScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            CachedNetworkImage(
+              imageUrl: 'https://ak.picdn.net/shutterstock/videos/1093975795/thumb/1.jpg',
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              progressIndicatorBuilder: (context, url, progress) =>
+              const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Center(
+                child: Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+              ),
+            ),
             const SizedBox(height: 20),
             ServiceSelector(
               services: services,
@@ -41,9 +56,13 @@ class HairRemovalScreen extends StatelessWidget {
               onDeleteService: onDeleteService,
             ),
             const SizedBox(height: 20),
-            ServiceDetails(
-              service: services[selectedService],
-            ),
+            Expanded(
+                child: SingleChildScrollView(
+                  child: ServiceDetails(
+                    service: services[selectedService],
+                  ),
+                )
+            )
           ],
         ),
       ),
