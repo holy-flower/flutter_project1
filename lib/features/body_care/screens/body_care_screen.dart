91db9180f9f9df2f22fef61d402428bc4f233c8d
holy-flower <prodolnova04@mail.ru>
2025-10-24 19:21:10 +0300

    добавление сетевого изображения на экран ухода за телом

--- lib/features/body_care/screens/body_care_screen.dart
+++ lib/features/body_care/screens/body_care_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/body_service.dart';
 import '../widgets/body_service_list.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class BodyCareScreen extends StatelessWidget {
   final List<BodyService> services;
@@ -32,6 +33,20 @@ class BodyCareScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            CachedNetworkImage(
+              imageUrl: 'https://avatars.mds.yandex.net/get-ydo/3927445/2a00000180bd9555624724006a173c0f80bd/diploma',
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              progressIndicatorBuilder: (context, url, progress) =>
+              const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Center(
+                child: Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+              ),
+            ),
             const SizedBox(height: 20),
             Expanded(
               child: BodyServiceList(
