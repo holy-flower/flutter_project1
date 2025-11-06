0aeb40ce4c7f611d1650d795c21a868304ea25af
holy-flower <prodolnova04@mail.ru>
2025-11-06 14:02:52 +0300

    изучение конструктора с большим перечнем полей

--- lib/features/newFile.dart
+++ lib/features/newFile.dart
@@ -1,7 +1,29 @@
 class MyClass {
-  const MyClass({required String arg1});
+  const MyClass({
+    required String arg1,
+    required String arg2,
+    required String arg3,
+    required String arg4,
+    required String arg5,
+    required String arg6,
+    required String arg7,
+    required String arg8,
+    required String arg9,
+    required String arg10,
+  });
 }
 
 void main(List<String> args) {
-  const obj = MyClass(arg1: 'arg');
+  const obj = MyClass(
+    arg1: 'arg',
+    arg2: 'arg2',
+    arg3: 'arg3',
+    arg4: 'arg4',
+    arg5: 'arg5',
+    arg6: 'arg6',
+    arg7: 'arg7',
+    arg8: 'arg8',
+    arg9: 'arg9',
+    arg10: 'arg10',
+  );
 }
